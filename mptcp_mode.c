2465075f5a4c81f67a52c0ba6ea2c45f34dacb1a
Per Hurtig <per.hurtig@kau.se>
2017-02-03 10:40:52 +0100

    changed socket option constant from SOL_TCP to IPPROTO_TCP

--- mptcp_mode.c
+++ mptcp_mode.c
@@ -11,7 +11,7 @@
 #define MPTCP_ENABLED 42
 #endif
 
-#define EXIT_ERROR	(1)
+#define EXIT_ERROR  (1)
 
 int (*std_socket)(int domain, int type, int protocol);
 
@@ -19,43 +19,43 @@ static void *libc_handle = NULL;
 
 void _mptcpmode_load()
 {
-	if (libc_handle != NULL)
-		return;
+    if (libc_handle != NULL)
+        return;
 
-	if (!(libc_handle = dlopen("libc.so", RTLD_LAZY))) {
-		if (!(libc_handle = dlopen("libc.so.6", RTLD_LAZY))) {
-			fprintf(stderr, "error loading libc!\n");
-			exit (EXIT_ERROR);
-		}
-	}
+    if (!(libc_handle = dlopen("libc.so", RTLD_LAZY))) {
+        if (!(libc_handle = dlopen("libc.so.6", RTLD_LAZY))) {
+            fprintf(stderr, "error loading libc!\n");
+            exit (EXIT_ERROR);
+        }
+    }
 
-	if (!(std_socket = dlsym(libc_handle, "socket"))) {
-		fprintf(stderr, "socket() not found in libc!\n");
-		exit (EXIT_ERROR);
-	}
+    if (!(std_socket = dlsym(libc_handle, "socket"))) {
+        fprintf(stderr, "socket() not found in libc!\n");
+        exit (EXIT_ERROR);
+    }
 }
 
 int socket(int domain, int type, int protocol)
 {
-	int enable = 0;
-	int fd;
-	char env_buff[256];
+    int enable = 0;
+    int fd;
+    char env_buff[256];
 
-	_mptcpmode_load();
+    _mptcpmode_load();
 
-	strcpy(env_buff, getenv("LD_PRELOAD_MPTCPMODE"));
+    strcpy(env_buff, getenv("LD_PRELOAD_MPTCPMODE"));
 
-	if(strcmp("on", env_buff) == 0)
-		enable = 1;
+    if(strcmp("on", env_buff) == 0)
+        enable = 1;
 
-	if (((PF_INET == domain) || (PF_INET6 == domain))
-	    && (SOCK_STREAM == type)) {
+    if (((PF_INET == domain) || (PF_INET6 == domain))
+        && (SOCK_STREAM == type)) {
 
-		fd = (std_socket)(domain, type, protocol);
-		setsockopt(fd, SOL_TCP, MPTCP_ENABLED, &enable, sizeof(enable));
+        fd = (std_socket)(domain, type, protocol);
+        setsockopt(fd, IPPROTO_TCP, MPTCP_ENABLED, &enable, sizeof(enable));
 
-		return fd;
-	}
+        return fd;
+    }
 
     return (std_socket)(domain, type, protocol);
 }
